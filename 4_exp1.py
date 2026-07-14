1c6d55b7ab442275b35b75ceb0f41d5b41f493f6
Gubbala Suchitra <25pa5a0518@vishnu.edu.in>
2026-07-14 15:26:14 +0530

    Create 4_exp1.py

---
+++ 4_exp1.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.arange(10) ** 3
+print("Let's consider the elements:", a)
+print("The output of a[2] is:", a[2])
+print("The output of a[2:4] is:", a[2:4])
+print("The output of a[2:3] is:", a[2:3])
+print("The output of a[-1] is:", a[-1])
+print("The output of a[-2] is:", a[-2])
+print("The output of a[-5:-2] is:", a[-5:-2])
